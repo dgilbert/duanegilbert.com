f3a623317ce831e7f37829399bd9336707de5bfb
Duane Gilbert <duane.gilbert@tresta.com>
2017-07-05 10:10:25 -0400

    Login spinner

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,125 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {withRouter, Link} from 'react-router-dom';
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
+import Routes from './Routes';
+import RouteNavItem from './components/RouteNavItem';
+import { CognitoUserPool, } from 'amazon-cognito-identity-js';
+import config from './config.js';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userToken: null,
+      isLoadingUserToken: true,
+    };
+  }
+
+  updateUserToken = (userToken) => {
+    this.setState({
+      userToken: userToken
+    });
+  }
+
+  isUserLoggedIn = () => {
+    const isUserLoggedIn = this.state.userToken !== null ? true : false;
+    console.info(isUserLoggedIn);
+    return isUserLoggedIn;
+  }
+
+  handleNavLink = (event) => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  }
+
+  handleLogout = (event) => {
+    const currentUser = this.getCurrentUser();
+
+    if (currentUser !== null) {
+      currentUser.signOut();
+    }
+
+    this.updateUserToken(null);
+
+    this.props.history.push('/login');
+  }
+
+  getCurrentUser() {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID
+    });
+    return userPool.getCurrentUser();
+  }
+
+  getUserToken(currentUser) {
+    return new Promise((resolve, reject) => {
+      currentUser.getSession(function(err, session) {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(session.getIdToken().getJwtToken());
+      });
+    });
+  }
+
+  componentDidMount = async () => {
+    const currentUser = this.getCurrentUser();
+
+    if (currentUser === null) {
+      this.setState({isLoadingUserToken: false});
+      return;
+    }
+
+    try {
+      const userToken = await this.getUserToken(currentUser);
+      this.updateUserToken(userToken);
+    }
+    catch(e) {
+      alert(e);
+    }
+
+    this.setState({isLoadingUserToken: false});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const childProps = {
+      userToken: this.state.userToken,
+      updateUserToken: this.updateUserToken,
+      isLoggedIn: this.isLoggedIn,
+    };
+    
+    return ! this.state.isLoadingUserToken
+    &&
+    (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              { this.isUserLoggedIn()
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : [ 
+                    <RouteNavItem key={1} onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>,
+                    <RouteNavItem key={2} onClick={this.handleNavLink} href="/login">Login</RouteNavItem> 
+                  ]
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ my-app/src/config.js
@@ -0,0 +1,12 @@
+export default {
+  cognito: {
+    USER_POOL_ID : 'us-east-1_fgpPDLGHc',
+    APP_CLIENT_ID : '1i8lkshj795sfabr8f6904sj2b',
+    IDENTITY_POOL_ID: 'us-east-1:c0d163d6-1790-4589-b5c6-ef0d0d82f533',
+  },
+
+  iam: {
+      ACCESS_KEY_ID: 'AKIAJ7INWSEAF2PBD3DQ',
+      SECRET_ACCESS_KEY: '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',
+  }
+};
