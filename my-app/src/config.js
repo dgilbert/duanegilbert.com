dea0b823557577884ce950e856710a6f28616c34
Duane Gilbert <duane.gilbert@tresta.com>
2017-07-05 23:39:28 -0400

    Delete config.js

--- my-app/src/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  cognito: {
-    USER_POOL_ID : 'us-east-1_fgpPDLGHc',
-    APP_CLIENT_ID : '1i8lkshj795sfabr8f6904sj2b',
-    IDENTITY_POOL_ID: 'us-east-1:c0d163d6-1790-4589-b5c6-ef0d0d82f533',
-  },
-};
