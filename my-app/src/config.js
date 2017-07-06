48e68973bc459e3616cc24f833ee84485e7a91a2
Duane Gilbert <duane.gilbert@tresta.com>
2017-07-05 21:34:49 -0400

    Removing access creds

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -4,9 +4,4 @@ export default {
     APP_CLIENT_ID : '1i8lkshj795sfabr8f6904sj2b',
     IDENTITY_POOL_ID: 'us-east-1:c0d163d6-1790-4589-b5c6-ef0d0d82f533',
   },
-
-  iam: {
-      ACCESS_KEY_ID: 'AKIAJ7INWSEAF2PBD3DQ',
-      SECRET_ACCESS_KEY: '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',
-  }
 };
